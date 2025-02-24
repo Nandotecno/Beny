ccacb605bae62d9c22017df865e6eda66fb72aca
Fernando Cruz <86312129+Nandotecno@users.noreply.github.com>
2025-02-24 06:25:18 -0300

    Add files via upload

---
+++ package com.caffeinealgorithm.java
@@ -0,0 +1,15 @@
+package com.caffeinealgorithm.programaremjava;
+
+public class Funcoes {
+  public void Run() {
+    dadosPessoais();
+    dadosPessoais();
+    dadosPessoais();
+  }
+
+  public void dadosPessoais() {
+    System.out.println("Nome: Nelson Silva");
+    System.out.println("Idade: 28");
+    System.out.println("Nacionalidade: Portuguesa");
+  }
+}
